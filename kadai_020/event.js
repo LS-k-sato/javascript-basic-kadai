c41a3feb1708b92d73b120bc69863beeec10ab6d
LS-k-sato <k_sato@leading-solutions.co.jp>
2024-02-01 18:25:40 +0900

    DOM操作を使用して文字表示を変えよう

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,3 +1,6 @@
-document.getElementById("btn").onclick = function() {
-  document.getElementById('text').innerHTML = "ボタンをクリックしました"
-};
+const Btn = document.getElementById('btn');
+const Text = document.getElementById('text');
+
+Btn.addEventListener('click', () => {
+  Text.innerHTML = "ボタンをクリックしました";
+})
